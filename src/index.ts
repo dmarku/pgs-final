43ba5f86e57c462eb1dbcd79eacbee5d811a2cb5
Markus Dittmann <dmarku@posteo.de>
2019-09-26 16:09:58 +0200

    add a simple particle system

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { GPUParticleSystem, MeshBuilder, Texture } from "@babylonjs/core";
 import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
@@ -21,5 +22,22 @@ SceneLoader.LoadAssetContainer("/", "test-scene.glb", scene, assets => {
 const cam = new ArcRotateCamera("name", 2, 0.7, 30, Vector3.Zero(), scene);
 cam.setTarget(Vector3.Zero());
 cam.attachControl(canvas, true);
+
+const emitterReference = MeshBuilder.CreateSphere(
+  "emitter reference",
+  {},
+  scene
+);
+emitterReference.position = new Vector3(0, 10, 0);
+
+const particles = new GPUParticleSystem("snow_v1", { capacity: 1000 }, scene);
+particles.start();
+particles.emitter = emitterReference;
+
+particles.particleTexture = new Texture("/snowflake.png", scene);
+
+particles.minEmitPower = 1;
+particles.maxEmitPower = 3;
+particles.updateSpeed = 0.005;
 
 engine.runRenderLoop(() => scene.render());
